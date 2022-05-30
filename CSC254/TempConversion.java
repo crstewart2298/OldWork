aec9032e7e2d94a02be0de6c0dfeec17b6b86577
crstewart2298 <106501872+crstewart2298@users.noreply.github.com>
2022-05-29 22:23:09 -0500

    Add files via upload

---
+++ CSC254/TempConversion.java
@@ -0,0 +1,14 @@
+public class TempConversion {
+    //Chris Stewart
+	//CSC184
+	//January 31, 2018
+
+	public static void main(String[] args) {
+
+		double celsius, fahrenheit;
+		System.out.println("Fahrenheit degree 113 in celsius is:");
+		System.out.println(celsius= (113 - 32) * (0.5556));
+		System.out.println("Celsius degree 31 in fahrenheit is:");
+		System.out.println(fahrenheit= (31 * 1.8) + 32);
+	}
+}
